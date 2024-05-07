2ca4194e6823b803eeb55c7577f7b915dbfe08b5
David Wheatley <david@davwheat.dev>
2024-05-07 09:16:07 +0100

    chore: updates

--- manual-linker.py
+++ manual-linker.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 
 def main():
-    autocomplete = {}
+    od_autocomplete = {}
 
     # Get all bus stops
     bus_stops = DBSession.query(BusStop).where(BusStop.audio_file.is_(None)).all()
@@ -30,8 +30,8 @@ def main():
         if section.origin_audio_file is None:
             origin_name = clean_name(section.origin_name)
 
-            if origin_name in autocomplete:
-                section.origin_audio_file = autocomplete[origin_name]
+            if origin_name in od_autocomplete:
+                section.origin_audio_file = od_autocomplete[origin_name]
                 section.origin_audio_file_likeliness = 999
                 continue
 
@@ -40,15 +40,15 @@ def main():
             )
 
             if len(filename) > 0:
-                autocomplete[origin_name] = filename
+                od_autocomplete[origin_name] = filename
                 section.origin_audio_file = filename
                 section.origin_audio_file_likeliness = 999
 
         if section.destination_audio_file is None:
             destination_name = clean_name(section.destination_name)
 
-            if destination_name in autocomplete:
-                section.destination_audio_file = autocomplete[destination_name]
+            if destination_name in od_autocomplete:
+                section.destination_audio_file = od_autocomplete[destination_name]
                 section.destination_audio_file_likeliness = 999
                 continue
 
@@ -57,9 +57,39 @@ def main():
             )
 
             if len(filename) > 0:
-                autocomplete[destination_name] = filename
+                od_autocomplete[destination_name] = filename
                 section.destination_audio_file = filename
                 section.destination_audio_file_likeliness = 999
+
+        DBSession.flush()
+        DBSession.commit()
+        DBSession.flush()
+
+    s_autocomplete = {}
+    print("Linking stops")
+
+    for stop in bus_stops:
+        n_id = stop.naptan_id
+        # Remove last character from naptan_id if it's a letter
+        if n_id[-1].isalpha():
+            n_id = n_id[:-1]
+
+        if stop.audio_file is None:
+            stop_name = clean_name(stop.common_name)
+
+            if n_id in s_autocomplete:
+                stop.audio_file = s_autocomplete[n_id]
+                stop.audio_file_likeliness = 999
+                continue
+
+            filename = input(
+                f"https://tfl.gov.uk/bus/stop/{stop.naptan_id} - Enter the filename for {stop_name}: "
+            )
+
+            if len(filename) > 0:
+                s_autocomplete[n_id] = filename
+                stop.audio_file = filename
+                stop.audio_file_likeliness = 999
 
         DBSession.flush()
         DBSession.commit()
